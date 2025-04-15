a7c29c6ba35421d7dad8f9ee94e1eeb380e93c2d
Sean Murphy <sean@iamseanmurphy.com>
2025-04-15 11:17:24 +0000

    1.0.13

--- riverbank/vendor/composer/installed.php
+++ riverbank/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/riverbank',
-        'pretty_version' => 'v1.0.12',
-        'version' => '1.0.12.0',
-        'reference' => '6e98ad854a4f6b391baee804ab98af20dcd004fe',
+        'pretty_version' => 'v1.0.13',
+        'version' => '1.0.13.0',
+        'reference' => '9e14682d54cf00f5561984a9775c720ffa1ebbc5',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/riverbank' => array(
-            'pretty_version' => 'v1.0.12',
-            'version' => '1.0.12.0',
-            'reference' => '6e98ad854a4f6b391baee804ab98af20dcd004fe',
+            'pretty_version' => 'v1.0.13',
+            'version' => '1.0.13.0',
+            'reference' => '9e14682d54cf00f5561984a9775c720ffa1ebbc5',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.37',
-            'version' => '3.3.37.0',
-            'reference' => '73e9010fb4e30ad8835f00a0df802733f98c4094',
+            'pretty_version' => '3.3.44',
+            'version' => '3.3.44.0',
+            'reference' => 'fed444b52ebf1f689ec2434df177926bf8f238c4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
